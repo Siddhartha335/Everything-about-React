5c4f3690e5d9e21d58db63eb2b62e3f3e513c6a5
Siddhartha335 <siddhartharaut33@gmail.com>
2024-05-22 21:22:57 +0545

    Used useState hook to manage states

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -1,36 +1,35 @@
-import './App.css'
-import Screen from './components/Screen'
-import Buttons from './components/Buttons'
-import ButtonsDown from './components/ButtonsDown'
-import { useState } from 'react'
+import "./App.css";
+import Screen from "./components/Screen";
+import Buttons from "./components/Buttons";
+import ButtonsDown from "./components/ButtonsDown";
+import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
 
-  const [calVal,setCalVal] = useState('')
-
-  const onButtonClick = (event)=> { 
-    const buttonText =(event.target.innerHTML);
-    if (buttonText =='C') {
-      setCalVal('')
-    } else if(buttonText == '=') {
-      const result = eval(calVal)
-      setCalVal(result)
+  const onButtonClick = (event) => {
+    const buttonText = event.target.innerHTML;
+    if (buttonText == "C") {
+      setCalVal("");
+    } else if (buttonText == "=") {
+      const results = eval(calVal);
+      setCalVal(results);
     } else {
       const newDisplay = calVal + buttonText;
-      setCalVal(newDisplay)
+      setCalVal(newDisplay);
     }
-  }
+  };
 
   return (
     <>
-      <h1 className='text-center mt-2 text-badge'>Calculator-App</h1>
-      <div id='calculator' className='container mt-4' >
-        <Screen calVal = {calVal}/>      
+      <h1 className="text-center mt-2 text-badge">Calculator-App</h1>
+      <div id="calculator" className="container mt-4">
+        <Screen calVal={calVal} />
         <Buttons onButtonClick={onButtonClick} />
-        <ButtonsDown onButtonClick={onButtonClick}/>
-        </div>
+        <ButtonsDown onButtonClick={onButtonClick} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- clock-app/src/App.jsx
+++ clock-app/src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import './App.css'
-import Heading from './components/Heading'
-import Intro from './components/Intro'
-import Time from './components/Time'
+import React from "react";
+import "./App.css";
 
 function App() {
-
-  let items = ['An item','A second item','A third item','A fourth item','And a fifth one']
+  let items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
 
   return (
     <>
-      {/* <Heading />
-      <Intro />
-      <Time /> */}
       <h1>Healthy foods</h1>
       <ul className="list-group">
-        {items.map((item,index)=> {
-          return <li className="list-group-item" key={index}>{item}</li>
+        {items.map((item, index) => {
+          return (
+            <li className="list-group-item" key={index}>
+              {item}
+            </li>
+          );
         })}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
