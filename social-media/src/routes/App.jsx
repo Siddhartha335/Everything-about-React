b5d5f75df5cd3594edeed175d76bd6f78fea3e6e
Siddhartha335 <siddhartharaut33@gmail.com>
2024-05-23 21:30:03 +0545

    Social media using Context API and useReducer hook

--- social-media/src/routes/App.jsx
+++ social-media/src/routes/App.jsx
@@ -2,9 +2,6 @@ import Navbar from "../components/Navbar";
 import "./App.css";
 import SideBar from "../components/SideBar";
 import Footer from "../components/Footer";
-import { useState } from "react";
-import CreatePost from "../components/CreatePost";
-import PostList from "../components/PostList";
 import PostListProvider from "../store/post-list-store";
 import { Outlet } from "react-router-dom";
 
@@ -19,7 +16,6 @@ function App() {
     <PostListProvider>
       <div className="sidebar">
         <div className="div1">
-          {/* <SideBar click={click} showChanges={showChanges} /> */}
           <SideBar/>
         </div>
         <div className="div2">
